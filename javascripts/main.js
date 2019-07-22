2870cca8e27f04a4c1c425889208db1aa032819d
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-22 19:03:52 +0200

    Logica di invio messaggio tramite tasto Spedisci

---
+++ javascripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+	// INVIO MESSAGGIO
+	// al click del tasto Spedisci
+	$(".fa-paper-plane").click(sendMessage);
+});
+
+function sendMessage() {
+	var textInput = $("#rcol__chat-input__textinput");
+	
+	// acquisisco contenuto text input e lo svuoto
+	var message = textInput.val();
+	textInput.val("");
+
+	// vado a prendere il template di aggiunta messaggi
+	var messageInHTML = $(".template .rcol__chat__message").clone();
+	// gli inserisco il messaggio acquisito
+	messageInHTML.text(message);
+
+	// aggiungo il messaggio pronto in HTML alla chat
+	$("#rcol__chat").append(messageInHTML);	
+}
